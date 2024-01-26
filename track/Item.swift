710d4a5a5d100a89ee32ef8cc34a121a68d43143
Sam Svindland <swsvindland@gmail.com>
2024-01-26 11:48:46 -0500

    Initial Commit

---
+++ track/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  track
+//
+//  Created by Sam Svindland on 1/26/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
